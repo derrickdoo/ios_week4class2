e5ac62d4c6fd171cdb20fa752d656dc54116d954
derricko <derricko@yahoo-inc.com>
2014-06-26 18:02:56 -0700

    sup yo

---
+++ week4class2/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  week4class2
+//
+//  Created by Derrick Or on 6/26/14.
+//  Copyright (c) 2014 derrickor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
